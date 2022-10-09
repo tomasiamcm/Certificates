7bafe0aa8eb0bd7ccf61358d4e936a6eab04477e
tomasia <https://github.com/tomasiamcm/DesafioDio_POO.git>
2022-10-08 23:04:25 -0300

    more exercicies

--- Mundo_2_Python/e2_037_conversorbasenumerica.py
+++ Mundo_2_Python/e2_037_conversorbasenumerica.py
@@ -1,7 +1,16 @@
-num = int(input('Informe um número entre 1 e 3: '))
+num = int(input('Informe um número: '))
 
+print('''Escolha uma das opções abaixo:
+[1] Converte para Binário
+[2] Converte para Octal
+[3] Converte para Hexadecimal
+''')
 
-1 == binario
-2 == octal
-3 = hexadecimal
+opcao = int(input('Digite a opção escolhida: '))
 
+if opcao == 1:
+    print('O {} convertido para Binário é {}.'.format(num, bin(num)[2:]))
+elif opcao == 2:
+    print('O {} convertido para Octal é {}.'.format(num, oct(num)[2:]))
+else:
+    print('O {} convertido para Hexadecimal [e {}.'.format(num, hex(num)[2:]))
