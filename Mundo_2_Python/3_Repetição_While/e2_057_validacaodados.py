83ef07ee4bfb5c6f74b48ff078f0e59b55ebec02
tomasia.mussuri <tomasia.mussuri@gmail.com>
2022-10-10 19:04:04 -0300

    update exercicies

--- Mundo_2_Python/3_Repetição_While/e2_057_validacaodados.py
+++ Mundo_2_Python/3_Repetição_While/e2_057_validacaodados.py
@@ -6,5 +6,5 @@ while sexo not in 'FM':
     sexo = str(input('Dados inválidos. Informe o sexo [F/M]: ')).strip().upper()[0]
     print('Só são aceitas as opções: F ou M. Tente novamente!')
     
-print('VOcê digitou {}.'.format(sexo))
+print('VOcê digitou o sexo {}.'.format(sexo))
  
