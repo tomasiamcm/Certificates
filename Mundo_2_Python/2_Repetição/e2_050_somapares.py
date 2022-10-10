b2d160b45dc6941afe175fa6089b7bd699c0c5a3
tomasia <https://github.com/tomasiamcm/DesafioDio_POO.git>
2022-10-09 23:21:05 -0300

    update exercicies

---
+++ Mundo_2_Python/2_Repetição/e2_050_somapares.py
@@ -0,0 +1,10 @@
+# Ler 6 números inteiros e mostre a soma dos valores pares.
+soma = 0
+cont = 0
+
+for c in range(1, 7):
+    n = int(input('Digite o {}º número: '.format(c)))
+    if n % 2 == 0:
+        soma += n
+        cont += 1
+print('Você informou {} números pares e a soma é {}.'.format(cont, soma))
