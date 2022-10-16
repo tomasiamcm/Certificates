471f1884c72c9b257b898553358b2463a75e2202
tomasia.mussuri <tomasia.mussuri@gmail.com>
2022-10-16 01:17:30 -0300

    update exercecies

--- Mundo_2_Python/4_Tuplas/e3_077_contandovogaisemtupla.py
+++ Mundo_2_Python/4_Tuplas/e3_077_contandovogaisemtupla.py
@@ -3,4 +3,10 @@ Crie um programa com uma tupla com várias palavras (não usar acentos).
 Mostre: as vogais de cada palavra.
 '''
 
-palavras = ('aprender', 'progrmar', '')
+palavras = ('APRENDER', 'PROGRAMAR', 'LINGUAGEM', 'PYTHON', 'CURSO', 'GRATIS'
+            'ESTUDAR', 'PRATICAR', 'TRABALHAR', 'MERCADO', 'PROGRAMADOR', 'FUTURO')
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} tem as vogais: ', end='')
+    for letra in p:
+        if letra.lower() in 'aeiou':
+            print(letra, end=' ')
